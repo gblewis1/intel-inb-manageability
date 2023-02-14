e6a39f038f3114a2e3e89b774e353bacaac4310a
Gavin Lewis <gavin.b.lewis@intel.com>
2023-02-14 11:22:32 -0800

    wip

---
+++ inbc-program/inbc/shared.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+    Shared module used by inbc-program
+
+    Copyright (C) 2020-2023 Intel Corporation
+    SPDX-License-Identifier: Apache-2.0
+"""
+
+running = True
+exit_code = 0
